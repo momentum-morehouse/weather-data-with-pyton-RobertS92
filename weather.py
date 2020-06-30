3a2070c12aac5eab0c03ec81dc1bef55e756d3c8
Robert Seals <rseals13@yahoo.com>
2020-06-30 20:50:57 +0000

    initial push

--- weather.py
+++ weather.py
@@ -1,49 +1,41 @@
 import requests
 
-location = [ { (32.965557, -96.715836), (32.907223, -96.635277), (32.705002, -97.122780 ), (30.266666, -97.733330), (32.779167, -96.808891), (29.749907, -95.358421), (31.845682, -102.367645), (26.203407, -98.230011), (	29.691063, -95.209099), (40.931210, -73.898750)
-}]
+location = [(32.965557,-96.715836), (32.907223,-96.635277), (32.705002, -97.122780 ), (30.266666,-97.733330), (32.779167,-96.808891), (29.749907, -95.358421), (31.845682,-102.367645), (26.203407,-98.230011), (	29.691063, -95.209099), (40.931210,-73.898750),]
+
+locationslon =  (  -96.715836,  -96.635277,  -97.122780 ,  -97.733330, -96.808891, -95.358421,  -102.367645,  -98.230011, 	 -95.209099, -73.898750)
+
+locationslad =  ( 32.965557,  32.907223,  32.705002, 30.266666, -97.733330, 32.779167,  29.749907,  31.845682,  26.203407,  	29.691063,  40.931210, )
 
 
-url = "https://api.climacell.co/v3/weather/forecast/daily"
 
-response = requests.request("GET", url)
+key = "xNV9aYu0PfZTPIXNRn4dzuixUKxYg1kx"
+
+class location_info:
+  def __init__(self, lat, lon):
+    self.lat = lat
+    self.lon = lon
+   
+
+def get_weather_data(list):
+  location = []
+  for item in list:
+    data = location_info(item[0], item[1])
+    location.append(data)
+    print(location)
+
+get_weather_data(location)
+
+
+url = "https://api.climacell.co/v3/weather/realtime"
+payload = {"apikey": "MlzJxYVk8vU6tjV0U4SNf9WKItn4fTzq", "lat":locationslad[0], "lon":locationslon[1],
+"unit_system": "us",
+"fields": [
+  "precipitation", "temp", "feels_like", "humidity"
+]}
+
+response = requests.get(url, params = payload)
 
 print(response.text)
 
 
-[
-  {
-    "precipitation": [
-      {
-        "observation_time": "2020-06-30T22:00:00Z",
-        "max": {
-          "value": 0.0295,
-          "units": "in/hr"
-        }
-      }
-    ],
-    "observation_time": {
-      "value": "2020-06-30"
-    },
-    "lat": 42.35544,
-    "lon": -71.05991
-  },
-  {
-    "precipitation": [
-      {
-        "observation_time": "2020-07-01T19:00:00Z",
-        "max": {
-          "value": 0.0517,
-          "units": "in/hr"
-        }
-      }
-    ],
-    "observation_time": {
-      "value": "2020-07-01"
-    },
-    "lat": 42.35544,
-    "lon": -71.05991
-  }
-]
 
- 
